31412c3547a0225e87e39981cbe1efbea810f6d1
yokinanya <1099332511@qq.com>
2023-05-30 19:58:22 +0800

    新增对正版玩家uuid的支持

--- README.md
+++ README.md
@@ -1,28 +1,16 @@
-因为懒兵英语不好，所以只有中文README。
+# Whitelist Manager
 
-LazyBing has pool english. chinese README only.
-
-
+一个基于 [MCDR-offline-whitelist-manager](https://github.com/Lazy-Bing-Server/MCDR-offline-whitelist-manager) 的插件，补全了对正版用户白名单的支持
 
 ## 起因
-
-在给基友开设离线 MC服务器时，发现即便添加了原版的白名单却仍然无法进入服务器。
-
-研究后发现：**原版白名单**即便在离线模式下，仍然以 **{正版UUID}** 记录白名单玩家；
-
-但是你登录时使用的是 **{离线UUID}** 显然，他和 `whitelist.json`中记录的UUID不符，导致无法进入。
-
-随后就有了此插件，设计为离线服管理白名单用。
-
-
-
+因服务器运营需要，由正版验证切换到基于 [MultiYggdrasil](https://github.com/YuxuanZuo/MultiYggdrasil) 实现的外置+正版共存验证时发现使用 `/whitelist add <player>` 添加的白名单变成了使用 version3 算法生成的盗版uuid，但是服务器是同时存在正版玩家的，所以需要一个能添加正版玩家白名单的插件。然后就想起了 [SinBing](https://github.com/Sinbing) 写的离线白名单插件，拿来稍微修改一下就能用了。
 
 
 ## 快速开始
 
 ### 	安装
 
-​		本插件为单文件，将本体 `OfflineWhitelistManager.py` 放入插件目录 `/plugin/` 可完成安装，
+​		本插件为单文件，将本体 `WhitelistManager.py` 放入插件目录 `/plugin/` 可完成安装，
 
 ​		随后需要使用MCDR命令`!!MCDR r plg`以载入插件。
 
@@ -39,14 +27,15 @@ LazyBing has pool english. chinese README only.
 ![hard_config](https://github.com/Lazy-Bing-Server/MCDR-offline-whitelist-manager/blob/main/pic/hard_config.png)
 
 
-
 ​		余下配置信息 **请不要** 修改，此处仅做介绍。
 
 ​		`offline_uuid_method: 获取离线UUID方法(int, 默认：3，推荐：3)`
 
-​		方法1：通过API获取离线UUID。
+​		**修改离线UUID获取方法不影响正版UUID的获取**，正版只支持通过方法1获取
 
-​		方法2：通过carpet bot + 比较 playerdata的方式获取离线UUID，需要服务器安装有Carpet Mod。		
+​		方法1：通过API获取UUID。
+
+​		方法2：通过carpet bot + 比较 playerdata的方式获取离线UUID，需要服务器安装有Carpet Mod。
 
 ​		**## 方法2对于同一昵称仅能添加一次白名单（意味着删白名等同于永久BAN）。不推荐 ##**
 
@@ -64,9 +53,9 @@ LazyBing has pool english. chinese README only.
 
 
 
-​		`!!wlist add [player name]` 
+​		`!!wlist add [player name] (online)`
 
-​			添加玩家至服务器白名单，**# 该命令需要MCDR权限ADMIN及以上 #**。
+​			添加玩家至服务器白名单，添加正版玩家时需要加上online参数(可缩写为o)，**# 该命令需要MCDR权限ADMIN及以上 #**。
 
 ​			![wlist_add](https://github.com/Lazy-Bing-Server/MCDR-offline-whitelist-manager/blob/main/pic/wlist_add.png)
 
@@ -90,30 +79,13 @@ LazyBing has pool english. chinese README only.
 
 ### 	备份
 
-​		每次操作白名单（Add/Remove)时都会生成一次备份，备份位于 `/server/whitelist_backup/`
+​		每次操作白名单(Add/Remove)时都会生成一次备份，备份位于 `/server/whitelist_backup/`
 
 ​		备份中会使用 **A / R** 记录本次备份时的操作类型 添加 / 删除 。（意味这备份是X操作前保存的）
 
 ​		![backup_whitelist](https://github.com/Lazy-Bing-Server/MCDR-offline-whitelist-manager/blob/main/pic/backup_whitelist.png)
 
 
-
-
-
 ## 鸣谢
 
-​	Ra1ny_Yuki: 在编写插件中作为人肉编译器及时用碳基生物自然语言骂我。
-
-​	Fallen_Breath: 从他那偷来了生成离线UUID的API。
-
-​	账号己注销: 提供了本地计算离线UUID的代码
-
-​	Tonashiki: 为我产生了编写该插件的需求。
-
-
-
-
-
-## 彩蛋
-
-​	本插件系强迫症友好型插件，插件大小为 8192 **Bytes**.
+​	[原仓库鸣谢列表](​https://github.com/DangoTown/MCDR-whitelist-manager#鸣谢) 以及 [SinBing](https://github.com/Sinbing)
---
+++ WhitelistManager.py
@@ -0,0 +1,248 @@
+# -*- code by: utf-8 -*-
+# original author: Sinbing.
+# edited by: yokina
+from urllib.request import urlopen
+from mcdreforged.api.all import *
+import hashlib
+import shutil
+import time
+import json
+import os
+import re
+
+DEBUG = False
+# Set [Minecraft Server Dir Name] and [World Dir Name] if changed.
+server_dirname = 'server'
+world_dirname = 'world'
+
+# 1: Caculate uuid from API  |  2: diff offline uuid from playerdata  |  3： local calculator by: 账号己注销  |
+uuid_method = 3
+bot_wait_time = 3
+
+
+PLUGIN_METADATA = {
+    'id': 'whitelist_manager',
+    'version': '1.0.0',
+    'name': 'Whitelist Manager',
+    'description': 'Manager whitelist when server using MultiYggdrasil.',
+    'author': 'yokina',
+    'link': 'https://github.com/yokinanya',
+    'dependencies': {
+        'mcdreforged': '>=2.0.0'
+    }
+}
+
+
+def server_cmd(info: Info, command: str):
+    if DEBUG:
+        print(f'CMD: {command}')
+    else:
+        info.get_server().execute(command)
+
+
+def Dprint(info: Info, text: str):
+    if DEBUG:
+        print(text)
+    else:
+        info.get_server().reply(info, text)
+
+
+def check_dir(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+
+def json_file_to_list(json_path: str) -> list:
+    with open(json_path, 'r', encoding='UTF-8') as file:
+        output_dict = json.load(file)
+    return output_dict
+
+
+def create_whitelist_file(json_list: list, workpath: str, type: str):
+    format_time = time.strftime('%y-%m-%d_%H-%M-%S', time.localtime())
+    # Backup old whitelist & create new whitelist.
+    check_dir(os.path.join(workpath, 'whitelist_backup'))
+    backup_whitelist_name = format_time + type + 'whitelist.json'
+    shutil.move(os.path.join(workpath, 'whitelist.json'), os.path.join(workpath, 'whitelist_backup', backup_whitelist_name))
+
+    # Create new whitelist.
+    file_name: str = os.path.join(workpath, 'whitelist.json')
+    with open(file_name, 'w', encoding='UTF-8') as file:
+        file.write(json.dumps(json_list, indent=4, ensure_ascii=False))
+
+
+def get_playerdata_filename_list():
+    filename_all = os.listdir(os.path.join(server_dirname, world_dirname, 'playerdata'))
+    playerdata_name_list = []
+    for filename in filename_all:
+        if filename[-4:] == '.dat':
+            playerdata_name_list.append(str(filename.split('.dat')[0]))
+    return playerdata_name_list
+
+
+def get_offline_uuid_m1(player_name: str):
+    flag = True
+    url = 'http://tools.glowingmines.eu/convertor/nick/' + player_name
+    try:
+        js = json.loads(urlopen(url, timeout=10).read().decode('utf8'))
+    except Exception as e:
+        flag = False
+    return js['offlinesplitteduuid'], flag
+
+
+def get_online_uuid_m1(player_name: str):
+    flag = True
+    url = 'http://tools.glowingmines.eu/convertor/nick/' + player_name
+    try:
+        js = json.loads(urlopen(url, timeout=10).read().decode('utf8'))
+    except Exception as e:
+        flag = False
+    return js['uuid'], flag
+
+
+def get_offline_uuid_m2(info: Info, player_name: str):
+    # Get playdata list.
+    uuid_list_old = get_playerdata_filename_list()
+
+    # Create offline playerdata by Carpet Bot.
+    server_cmd(info, f'/player {player_name} spawn')
+    time.sleep(bot_wait_time)
+    server_cmd(info, f'/player {player_name} kill')
+
+    # Diff playerdata filename list.
+    diff_uuid, count = '', 0
+    uuid_list_new = get_playerdata_filename_list()
+    for uuid in uuid_list_new:
+        if uuid not in uuid_list_old:
+            count = count + 1
+            diff_uuid, Flag = uuid, True
+    if diff_uuid == '' or count > 1:
+        Flag = False
+        Dprint(info, '未找到UUID，请重试')
+    return diff_uuid, Flag
+
+
+def get_offline_uuid_m3(player_name: str):
+    # Thanks for [账号己注销 - BiliUID:5995861]
+    md5Bytes = bytearray(hashlib.md5(b'OfflinePlayer:'+player_name.encode()).digest())
+    md5Bytes[6] &= 0x0f    # clear version
+    md5Bytes[6] |= 0x30    # set to version 3
+    md5Bytes[8] &= 0x3f    # clear variant
+    md5Bytes[8] |= 0x80    # set to IETF variant
+    offline_uuid = md5Bytes.hex()[:8]+ '-'+ md5Bytes.hex()[8:12]+ '-'+ md5Bytes.hex()[12:16]+ '-'+ md5Bytes.hex()[16:20]+ '-'+ md5Bytes.hex()[20:]
+    return offline_uuid, True
+
+
+def add_whitelist(info: Info, player_name: str, online: bool):
+    # Get whitelist name.
+    whitelist_list: list = json_file_to_list(os.path.join(server_dirname, 'whitelist.json'))
+    whitelist_name_list = []
+    for value in whitelist_list:
+        whitelist_name_list.append(value['name'])
+
+    # Check player name in whitelist.
+    if player_name in whitelist_name_list:
+        Dprint(info, f'添加 {player_name} 白名时出现错误：白名单中已存在该玩家。')
+    else:
+
+    # Get uuid.
+        if online == True:
+            uuid = get_online_uuid_m1(player_name)
+        else:
+            if uuid_method == 1:
+                uuid = get_offline_uuid_m1(player_name)
+            elif uuid_method == 2:
+                uuid = get_offline_uuid_m2(info, player_name)
+            elif uuid_method == 3:
+                uuid = get_offline_uuid_m3(player_name)
+
+    # Add whitelist.
+        if uuid[1]:
+            new_whitelist_dict = {'uuid': uuid[0], 'name': player_name}
+            whitelist_list.append(new_whitelist_dict)
+            create_whitelist_file(whitelist_list, os.path.join(server_dirname), '_A_')
+            time.sleep(1)
+            server_cmd(info, '/whitelist reload')
+            Dprint(info, f'\n|  白名单已添加新玩家:\n|  name: {player_name}\n|  UUID: {uuid[0]}')
+        else:
+            Dprint(info, f'添加 {player_name} 白名时出现错误，请重试。')
+
+
+def remove_whitelist(info: Info, player_name: str):
+    # Get whitelist name.
+    whitelist_list: list = json_file_to_list(os.path.join(server_dirname, 'whitelist.json'))
+    for value in whitelist_list:
+        if player_name == value['name']:
+            sub_dict = {'uuid': value['uuid'], 'name': value['name']}
+            whitelist_list.remove(sub_dict)
+
+    # Remove whitelist.
+    create_whitelist_file(whitelist_list, os.path.join(server_dirname), '_R_')
+    Dprint(info, f'白名单已移除玩家: name: {player_name}.')
+
+
+def show_whitelist(info: Info):
+    # Get whitelist name.
+    whitelist_list: list = json_file_to_list(os.path.join(server_dirname, 'whitelist.json'))
+    Dprint(info, '===== 服务器白名单 =====：')
+    for i in range(0, len(whitelist_list)):
+        player_name, player_uuid = whitelist_list[i]['name'], whitelist_list[i]['uuid']
+        Dprint(info, f'{i+1}: {player_name} - {player_uuid}')
+
+
+def show_help_msg(info: Info):
+    Dprint(info, '''
+    ============== 白名插件 ===============
+    !!wlist add [name] (online) 添加白名单
+    !!wlist remove [name]       移除白名单
+    !!wlist list                显示完整白名单
+    !!wlist help                显示此帮助信息
+    ======================================''')
+
+
+# MCDR.
+def on_user_info(server: PluginServerInterface, info: Info):
+    if info.content.startswith('!!wlist'):
+        args = info.content.split(' ')
+
+        try:
+            if args[1] == 'add':
+                if server.get_permission_level(info) >= 3:
+                    if args[2] != '':
+                        if args[3] == '':
+                            online = False
+                        elif re.search('o|online', args[3]):
+                            online = True
+                        else:
+                            online = False
+                        add_whitelist(info, args[2],online)
+                    else:
+                        Dprint(info, '你不告诉我名字我加啥啊')
+                else:
+                    Dprint(info, '你想屁吃')
+
+            elif args[1] == 'remove':
+                if server.get_permission_level(info) >= 3:
+                    if args[2] != '':
+                        remove_whitelist(info, args[2])
+                    else:
+                        Dprint(info, '你不告诉我名字我删谁啊')
+                else:
+                    Dprint(info, '你想屁吃')
+
+            elif args[1] == 'list':
+                if server.get_permission_level(info) >= 3:
+                    show_whitelist(info)
+                else:
+                    Dprint(info, '你想屁吃')
+
+            elif args[1] == 'help':
+                show_help_msg(info)
+            else:
+                Dprint(info, f'解析命令树第二项时发生BUG.')
+        except IndexError:
+            show_help_msg(info)
+
+
+def on_load(server: PluginServerInterface, prev):
+    server.register_help_message('!!wlist', '白名插件')
